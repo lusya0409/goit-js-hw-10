c699497287698709ab5b89e2cca3a6dccf411b04
lusya0409 <lusya0409@gmail.com>
2023-07-01 19:56:25 +0300

    changes hw10

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// 'api_key=live_DjX7ZaLbQUvAneYUcJq6j8upL76nJFzn6j1gyGmihfQw9rcXCpAK4iPLqb2VnXfK';
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_DjX7ZaLbQUvAneYUcJq6j8upL76nJFzn6j1gyGmihfQw9rcXCpAK4iPLqb2VnXfK';
+
+// https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME
